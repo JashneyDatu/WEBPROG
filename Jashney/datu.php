f4b3373b341e19f6aebfb8edff75a8cbe377f48f
JashneyDatu <jashneykurtd@gmail.com>
2025-09-16 14:35:08 +0800

    Add files via upload

---
+++ Jashney/datu.php
@@ -0,0 +1,31 @@
+<?php
+  echo "My name is: Jashney Kurt Datu<br>";
+  echo "I am 19 years old<br>";
+  echo "My favorite subject is: Mathematics<br>";
+  echo "My dream job is: VetMed<br>";
+  echo "My motto in life: 'Its Better to Give than to Receive.'<br>";
+  echo "Here is my ASCII Art.<br>";
+  echo "      .--..--..--..--..--..--.<br>";
+  echo "    .' \  (`._   (_)     _   \<br>";
+  echo "  .'    |  '._)         (_)  |<br>";
+  echo "  \ _.')\      .----..---.   /<br>";
+  echo "  |(_.'  |    /    .-\-.  \  |<br>";
+  echo "  \     0|    |   ( O| O) | o|<br>";
+  echo "   |  _  |  .--.____.'._.-.  |<br>";
+  echo "   \ (_) | o         -` .-`  |<br>";
+  echo "    |    \   |`-._ _ _ _ _\ /<br>";
+  echo "    \    |   |  `. |_||_|   |<br>";
+  echo "    | o  |    \_      \     |     -.   .-.<br>";
+  echo "    |.-.  \     `--..-'   O |     `.`-' .'<br>";
+  echo "  _.'  .' |     `-.-'      /-.__   ' .-'<br>";
+  echo ".' `-.` '.|='=.='=.='=.='=|._/_ `-'.'<br>";
+  echo "`-._  `.  |________/\_____|    `-.'.<br>";
+  echo "   .'   ).| '=' '='\/ '=' |<br>";
+  echo "   `._.`  '---------------'<br>";
+  echo "           //___\   //___\<br>";
+  echo "             ||       ||<br>";
+  echo "             ||_.-.   ||_.-.<br>";
+  echo "            (_.--__) (_.--__)<br>";
+
+
+ ?>
---
+++ Jashney/name.php
@@ -0,0 +1,5 @@
+<?php
+$name = "Jashney Datu";
+ $age = 19;
+ echo "My name is $name and I am $age years old";
+?>
